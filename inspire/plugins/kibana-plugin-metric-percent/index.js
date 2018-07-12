f2edbedafa2bd0782a209eba8cb0aa28c58adada
valentinab25 <valentinab25>
2018-07-12 11:58:48 +0300

    remove unused plugin

--- inspire/plugins/kibana-plugin-metric-percent/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function (kibana) {
-  return new kibana.Plugin({
-    uiExports: {
-      visTypes: ['plugins/metric_percent_vis/metric_percent_vis']
-    }
-  });
-};
